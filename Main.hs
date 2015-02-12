817958ad2ea06d0e65c725cd2d0051674e589be0
Mikhail Puzanov <mpuzanov@ripe.net>
2015-02-12 22:06:15 +0100

    Add dependencies, add initial happstack app.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,9 @@
 
-main = do 
-  print "I publish stuff"
+import Control.Monad
+import Happstack.Server ( Method(POST), Response, ServerPart, dir, method, toResponse
+                        , nullConf, ok, simpleHTTP, seeOther, path
+                        )
+
+main :: IO ()
+main = simpleHTTP nullConf $ ok (toResponse "Hello, World!") 
 
