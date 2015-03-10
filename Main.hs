48c860a0fcaecd10c79c5d81268e0012effe1167
Mikhail Puzanov <misha.puzanov@gmail.com>
2015-03-10 11:56:27 -0700

    Add some minimal shared state handling.

--- Main.hs
+++ Main.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
 import Control.Monad (msum)
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
 import Control.Monad.IO.Class (liftIO)
-import Happstack.Server (ServerPart, Response, takeRequestBody, askRq, ok, dir, 
+import Control.Monad.Trans.Class (lift)
+import Happstack.Server (ServerPart, ServerPartT, Response, takeRequestBody, askRq, ok, dir, 
                          method, unBody, toResponse, Method(POST))
 import Happstack.Server.Types
 import Happstack.Server.Env (simpleHTTP)
@@ -10,18 +14,27 @@ import qualified Data.ByteString.Lazy.Char8 as L
 import Repo
 
 main :: IO ()
-main = simpleHTTP nullConf $ msum 
-  [ do dir "message" $ do method POST 
-                          processMessage ]
+main = do
+    appState <- atomically $ newTVar emptyRepo
+    simpleHTTP nullConf $ msum 
+      [ do dir "message" $ do method POST 
+                              processMessage appState
+      ]
 
-
-processMessage :: ServerPart L.ByteString
-processMessage = do 
+processMessage :: TVar (Maybe Repository) -> ServerPart L.ByteString
+processMessage appState = do 
     req  <- askRq
     body <- liftIO $ takeRequestBody req
     case body of 
-      Just rqbody -> return . unBody $ rqbody 
+      Just rqbody -> lift . (getResponse appState) . unBody $ rqbody 
       Nothing     -> return ""
+    where
+        getResponse :: TVar (Maybe Repository) -> L.ByteString -> IO L.ByteString
+        getResponse appState request = atomically $ do
+            state <- readTVar appState
+            return $ case state of
+                Nothing -> "nothing"
+                Just s -> response s request
 
-response :: L.ByteString -> L.ByteString
-response xml = xml
+response :: Repository -> L.ByteString -> L.ByteString
+response r xml = xml
--- src/Repo.hs
+++ src/Repo.hs
@@ -1,21 +1,43 @@
 module Repo where
 
-import Data.UUID
+import Data.UUID as UU
 import Network.URI
 
-data SnapshotDef = Snapshot {
-    snapshotSession_id :: UUID
-  , snapshotSerial :: Integer
-  , snapshotUri :: URI
+newtype Serial = Serial Integer
+newtype Version = Version Int
+newtype SessionId = SessionId UUID
+newtype Hash = Hash String
+newtype Base64 = Base64 String
+
+data SnapshotDef = SnapshotDef UUID Serial URI 
+
+data DeltaDef = DeltaDef Serial URI
+
+data Notification = Notification Version Serial SnapshotDef [DeltaDef]
+
+data Snapshot = Snapshot SnapshotDef [SnapshotPublish]
+data Delta = Delta [DeltaPublish] [Withdraw]
+
+data SnapshotPublish = SnapshotPublish URI Base64
+data DeltaPublish = DeltaPublish URI Hash Base64
+
+data Withdraw = Withdraw URI Hash
+
+-- TODO Force snapshot and deltas to have the same session id
+data Repository = Repository Snapshot [Delta]
+
+data AppState = AppState {
+  repository :: Repository
 }
 
-data DeltaDef = DeltaDef {
-    deltaSerial :: Integer
-  , deltaUri :: URI  
-}
-
-data Notification = Notification {
-    version :: Int    
-}
-
-
+emptyRepo :: Maybe Repository
+emptyRepo = do
+    uuid <- UU.fromString "c2cc10e1-57d6-4b6f-9899-38d972112d8c"
+    return $ Repository
+          ( Snapshot
+            (SnapshotDef
+              uuid
+              (Serial 1)
+              nullURI)
+            [])
+          []
