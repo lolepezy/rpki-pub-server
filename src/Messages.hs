66f58af491306b948bb90f9625bb5664af5b4e86
Mikhail Puzanov <misha.puzanov@gmail.com>
2015-03-12 17:45:51 -0700

    Added proper error handling in message parsing.

--- src/Messages.hs
+++ src/Messages.hs
@@ -1,14 +1,15 @@
 module Messages where
 
-import Network.URI
-import Text.XML.Light
+import           Network.URI
 
-import qualified Data.ByteString.Lazy.Char8  as L
+import           Text.XML.Light
 
-newtype Version = Version Int 
+import qualified Data.ByteString.Lazy.Char8 as L
+
+newtype Version = Version Int
   deriving (Show, Eq)
-  
-newtype Base64 = Base64 String 
+
+newtype Base64 = Base64 String
   deriving (Show, Eq)
 
 data MType = Query | Reply
@@ -17,45 +18,57 @@ data MType = Query | Reply
 data Message = Message Version MType [Pdu]
   deriving (Show, Eq)
 
-data Pdu = Publish URI Base64 
-  | Withdraw URI 
+data Pdu = Publish URI Base64
+  | Withdraw URI
   deriving (Show, Eq)
 
-data Error = BadXml String 
-  | NoVersion 
+data Error = BadXml String
+  | NoVersion
   | NoMessageType
   | BadMessageType String
+  | BrokenPublishElement
+  | BrokenWithdrawElement
   | NoURI
   | BadURI String
 
 parseMessage :: L.ByteString -> Either Error Message
 parseMessage xml = do
-  doc         <- maybeToEither (BadXml $ L.unpack xml) (parseXMLDoc xml)
-  version     <- maybeToEither NoVersion $ lookupAttr (qname "version") (elAttribs doc)
-  messageType <- maybeToEither NoMessageType $ lookupAttr (qname "type") (elAttribs doc)
-  mType <- case messageType of
-                "query" -> Right Query
-                "reply" -> Right Reply
-                _       -> Left $ BadMessageType messageType
+    doc         <- maybeToEither (BadXml $ L.unpack xml) (parseXMLDoc xml)
+    version     <- maybeToEither NoVersion $ lookupAttr (qname "version") (elAttribs doc)
+    messageType <- maybeToEither NoMessageType $ lookupAttr (qname "type") (elAttribs doc)
+    mType       <- case messageType of
+                        "query" -> Right Query
+                        "reply" -> Right Reply
+                        _       -> Left $ BadMessageType messageType
 
-  let publishPdu = [ toPublish (lookupAttr (qname "uri") attrs) base64 | 
-                     Element { elAttribs = attrs,  elContent = [Text CData { cdData = base64 }]} <- findElements (qname "publish") doc ]      
-                      
-  let withdrawPdu = [ toWithdraw $ lookupAttr (qname "uri") attrs | Element { elAttribs = attrs } <- findElements (qname "withdraw") doc ]      
+    let publishPdus = map (\e -> 
+            case e of 
+                Element { elAttribs = attrs,  elContent = [Text CData { cdData = base64 }]} -> toPublish (lookupAttr (qname "uri") attrs) base64
+                _                                                                           -> Left BrokenPublishElement
+            ) $ findElements (qname "publish") doc
 
-  pdus <- sequence $ publishPdu ++ withdrawPdu      
-  return $ Message (Version (read version)) mType pdus
-  
-  where 
-      qname n = QName { qName = n, qURI = Nothing, qPrefix = Nothing }      
-      toPublish Nothing _ = Left NoURI
-      toPublish (Just u) base64 = do
-        uri <- maybeToEither (BadURI u) (parseURI u)
-        return $ Publish uri (Base64 base64)
-      toWithdraw Nothing = Left NoURI
-      toWithdraw (Just u) = do
-        uri <- maybeToEither (BadURI u) (parseURI u)
-        return $ Withdraw uri
+    let withdrawPdus = map (\e -> 
+            case e of  
+                Element { elAttribs = attrs } -> toWithdraw $ lookupAttr (qname "uri") attrs
+                _                             -> Left BrokenWithdrawElement 
+            ) $ findElements (qname "withdraw") doc
+
+    pdus <- sequence $ publishPdus ++ withdrawPdus
+    return $ Message (Version (read version)) mType pdus
+    where
+        qname n = QName { qName = n, qURI = Nothing, qPrefix = Nothing }
+
+        toPublish :: Maybe String -> String -> Either Error Pdu
+        toPublish Nothing _ = Left NoURI
+        toPublish (Just u) base64 = do
+            uri <- maybeToEither (BadURI u) (parseURI u)
+            return $ Publish uri (Base64 base64)
+
+        toWithdraw :: Maybe String -> Either Error Pdu
+        toWithdraw Nothing = Left NoURI
+        toWithdraw (Just u) = do
+            uri <- maybeToEither (BadURI u) (parseURI u)
+            return $ Withdraw uri
 
 
 maybeToEither :: e -> Maybe a -> Either e a
