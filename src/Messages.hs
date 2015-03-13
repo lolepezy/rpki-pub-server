bb8d6086f5f70aa50155f55d7a95761514c3c012
Mikhail Puzanov <misha.puzanov@gmail.com>
2015-03-13 15:49:06 -0700

    Refactor message types a bit.

--- src/Messages.hs
+++ src/Messages.hs
@@ -14,18 +14,21 @@ newtype Base64 = Base64 String
 data MType = Query | Reply
   deriving (Show, Eq)
 
-data Message = Message Version MType [Pdu]
+data Message = QueryMessage Version [QueryPdu] 
+             | ReplyMessage Version [ReplyPdu] 
   deriving (Show, Eq)
 
-data Pdu = Publish URI Base64
-  | Withdraw URI
+data QueryPdu = PubkishQ URI Base64 | WithdrawQ URI
+  deriving (Show, Eq)
+  
+data ReplyPdu = PublishR URI | WithdrawR URI
   deriving (Show, Eq)
 
 data Error = BadXml String
   | NoVersion
   | NoMessageType
   | BadMessageType String
-  | BrokenElement
+  | UnexpectedElement String
   | NoURI
   | BadURI String
 
@@ -33,43 +36,41 @@ parseMessage :: L.ByteString -> Either Error Message
 parseMessage xml = do
     doc         <- maybeToEither (BadXml $ L.unpack xml) (parseXMLDoc xml)
     version     <- maybeToEither NoVersion $ lookupAttr (qname "version") (elAttribs doc)
-    messageType <- maybeToEither NoMessageType $ lookupAttr (qname "type") (elAttribs doc)
-    mType       <- case messageType of
-                        "query" -> Right Query
-                        "reply" -> Right Reply
-                        _       -> Left $ BadMessageType messageType
-
-    pdus <- sequence $ map (\element -> 
+    pdus <- mapM (\element -> 
             case element of 
                 Element { elName = QName { qName = "publish" },
                           elAttribs = attrs,  
                           elContent = [Text CData { cdData = base64 }]
                         } -> toPublishPdu (lookupAttr (qname "uri") attrs) base64
-                        
+
                 Element { elName = QName { qName = "withdraw" }, 
                           elAttribs = attrs 
                         } -> toWithdrawPdu $ lookupAttr (qname "uri") attrs
-                        
-                _         -> Left BrokenElement 
+
+                _         -> Left $ UnexpectedElement (strContent element)
             ) $ elChildren doc
-            
-    return $ Message (Version (read version)) mType pdus
+
+    return $ QueryMessage (Version (read version)) pdus
     where
         qname name = QName { qName = name, qURI = Nothing, qPrefix = Nothing }
 
-        toPublishPdu :: Maybe String -> String -> Either Error Pdu
+        toPublishPdu :: Maybe String -> String -> Either Error QueryPdu
         toPublishPdu Nothing _ = Left NoURI
         toPublishPdu (Just u) base64 = do
             uri <- maybeToEither (BadURI u) (parseURI u)
-            return $ Publish uri (Base64 base64)
+            return $ PubkishQ uri (Base64 base64)
 
-        toWithdrawPdu :: Maybe String -> Either Error Pdu
+        toWithdrawPdu :: Maybe String -> Either Error QueryPdu
         toWithdrawPdu Nothing = Left NoURI
         toWithdrawPdu (Just u) = do
             uri <- maybeToEither (BadURI u) (parseURI u)
-            return $ Withdraw uri
+            return $ WithdrawQ uri
+
+
+--replyToMessage :: Message -> 
 
 
 maybeToEither :: e -> Maybe a -> Either e a
 maybeToEither e Nothing  = Left e
 maybeToEither _ (Just a) = Right a
+
