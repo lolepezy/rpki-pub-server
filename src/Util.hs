14ca44b3b050656d0cc74342bf99ec837c7fb6bb
Mikhail Puzanov <mpuzanov@ripe.net>
2015-03-25 22:09:04 +0100

    Make proper SHA256 formatting.

--- src/Util.hs
+++ src/Util.hs
@@ -3,6 +3,7 @@ module Util where
 import qualified Data.ByteString.Lazy.Char8  as L
 import qualified Data.ByteString.Char8  as C
 import qualified Data.ByteString.Base64.Lazy as B64
+import qualified Data.ByteString.Base16 as B16
 import qualified Crypto.Hash.SHA256 as SHA256
 
 import Types
@@ -13,7 +14,7 @@ maybeToEither _ (Just a) = Right a
 
 getHash :: Base64 -> Either ParseError Hash
 getHash (Base64 base64) = case B64.decode $ L.pack base64 of
-   Right b64 -> Right $ Hash . C.unpack . SHA256.hashlazy $ b64
+   Right b64 -> Right $ Hash . C.unpack . B16.encode . SHA256.hashlazy $ b64
    Left error -> Left $ BadBase64 error
 
 
