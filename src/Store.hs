2121e7c8117625031f9ca0987a46e03f46ecad2d
Mikhail Puzanov <mpuzanov@ripe.net>
2015-12-27 20:46:22 +0100

    Add latestSynced serial.

--- src/Store.hs
+++ src/Store.hs
@@ -35,8 +35,13 @@ instance Indexable RepoObject where
       ixGen (IX.Proxy :: IX.Proxy URI)
     ]
 
-data Repo = Repo SessionId Serial (IxSet RepoObject) (IxSet Delta)
-  deriving (Show, Typeable, Data)
+data Repo = Repo {
+  sessionId :: SessionId,
+  serial :: Serial,
+  objects :: IxSet RepoObject,
+  deltas :: IxSet Delta,
+  latestSynced :: Serial
+} deriving (Show, Typeable, Data)
 
 $(deriveSafeCopy 0 'base ''Repo)
 
@@ -52,7 +57,7 @@ instance Indexable Delta where
 
 $(deriveSafeCopy 0 'base ''Deltas)
 
-type StoredData = (SessionId, Serial, [RepoObject], [Delta])
+type RepoState = (SessionId, Serial, [RepoObject], [Delta], Serial)
 
 getByClientId :: ClientId -> Query Repo [RepoObject]
 getByClientId = getByA
@@ -64,61 +69,67 @@ getByURI uri = do
 
 getByURIs :: [URI] -> Query Repo [RepoObject]
 getByURIs uris = do
-  Repo _ _ objs _ <- ask
+  Repo { objects = objs } <- ask
   return $ toList $ objs @+ uris
 
 getByA :: Typeable a => a -> Query Repo [RepoObject]
 getByA f = do
-  Repo _ _ objs _ <- ask
+  Repo { objects = objs } <- ask
   return $ toList $ objs @= f
 
-getInfo :: Query Repo (SessionId, Serial)
+getInfo :: Query Repo (SessionId, Serial, Serial)
 getInfo = do
-  Repo sessionId serial _ _  <- ask
-  return (sessionId, serial)
+  Repo { sessionId = sId, serial = s, latestSynced = ls } <- ask
+  return (sId, s, ls)
 
 getAllObjects :: Query Repo [RepoObject]
 getAllObjects = do
-  Repo _ _ objs _ <- ask
+  Repo { objects = objs } <- ask
   return $ toList objs
 
 getDelta :: Serial -> Query Repo (Maybe Delta)
 getDelta serial = do
-  Repo _ _ _ deltas <- ask
-  return $ listToMaybe $ toList $ deltas @= serial
+  Repo { deltas = d }  <- ask
+  return $ listToMaybe $ toList $ d @= serial
 
-getRepo :: Query Repo StoredData
+getRepo :: Query Repo RepoState
 getRepo = do
-  Repo sessionId serial objects deltas  <- ask
-  return (sessionId, serial, toList objects, toList deltas)
+  Repo sessionId serial objects deltas latest <- ask
+  return (sessionId, serial, toList objects, toList deltas, latest)
 
 removeDelta :: Serial -> Update Repo Repo
 removeDelta serial = do
-  Repo sessionId rSerial objs deltas <- get
-  let r1 = Repo sessionId rSerial objs $ IX.deleteIx serial deltas
+  r @ Repo { deltas = d } <- get
+  let r1 = r { deltas = IX.deleteIx serial d }
   put r1
   return r1
 
 
 applyActions :: ClientId -> Delta -> [Action] -> Update Repo Repo
 applyActions cId delta actions = do
-  Repo sessionId serial objects deltas <- get
+  oldR @(Repo _ s objects deltas _) <- get
   -- TODO replace Add and Update with just one of them
   let toDelete = fromList [ uri | Delete_ uri <- actions ]
   let adds    = [ RepoObject { uri = u, base64 = base64, clientId = cId } | Add_  (u, base64) <- actions, keep u toDelete ]
   let updates = [ RepoObject { uri = u, base64 = base64, clientId = cId } | Update_ (u, base64) <- actions, keep u toDelete ]
   let newR = foldl (\accum r -> IX.updateIx (uri r) r accum) objects $ adds ++ updates
   let newD = IX.updateIx serial delta deltas
-  let repo = Repo sessionId (next serial) newR newD
+  let repo = oldR { serial = next s, objects = newR, deltas = newD }
   put repo
   return repo
   where
     next (Serial s) = Serial (s + 1)
     keep u toDelete = not $ u `member` toDelete
 
+markSync :: Update Repo Repo
+markSync = do
+  oldR @ Repo { serial = s } <- get
+  let repo = oldR { latestSynced = s }
+  put repo
+  return repo
 
 initialStore :: SessionId -> Repo
-initialStore sId = Repo sId (Serial 0) IX.empty IX.empty
+initialStore sId = Repo sId (Serial 0) IX.empty IX.empty (Serial 0)
 
-$(makeAcidic ''Repo [ 'getByURI, 'getByURIs, 'getInfo, 'getDelta, 'removeDelta, 'getAllObjects, 'getRepo, 'applyActions ])
+$(makeAcidic ''Repo [ 'getByURI, 'getByURIs, 'getInfo, 'getDelta, 'removeDelta, 'getAllObjects, 'getRepo, 'applyActions, 'markSync ])
 $(makeAcidic ''Deltas [ ])
