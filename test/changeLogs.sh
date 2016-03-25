5023d7c6fb28c2d076079703d3898a5ba211592a
Mikhail Puzanov <mpuzanov@ripe.net>
2016-03-25 16:34:28 +0100

    Add measurement results.

--- test/changeLogs.sh
+++ test/changeLogs.sh
@@ -1,46 +1,33 @@
 #!/bin/sh
 
 function measure() {
-  ./changeLogList +RTS -N8 -RTS --threads=$1 --changes=$2 --use-change-log=$3 2>changeLog.txt
-  TOTAL=`cat changeLog.txt | grep process | wc -l`
+  ./changeLogList +RTS -N8 -RTS --threads=$1 --changes=$2 --use-change-log=false 2>changeLog.txt
+  WO_TOTAL=`cat changeLog.txt | grep process | wc -l`
   UNIQUE=`cat changeLog.txt | grep process | sort | uniq | wc -l`
-  echo $1 $2 $3 $TOTAL $UNIQUE
+  ./changeLogList +RTS -N8 -RTS --threads=$1 --changes=$2 --use-change-log=true 2>changeLog.txt
+  W_TOTAL=`cat changeLog.txt | grep process | wc -l`
+  echo $1 $2 $UNIQUE $WO_TOTAL $W_TOTAL
 }
 
-measure 5 20 true
-measure 5 30 true
-measure 5 40 true
-measure 5 50 true
-measure 5 80 true
-measure 5 100 true
+measure 5 20
+measure 5 30
+measure 5 40
+measure 5 50
+measure 5 80
+measure 5 100
 
-measure 5 20 false
-measure 5 30 false
-measure 5 40 false
-measure 5 50 false
-measure 5 80 false
-measure 5 100 false
+measure 10 20
+measure 10 30
+measure 10 40
+measure 10 50
+measure 10 80
+measure 10 100
 
-measure 10 20 true
-measure 10 30 true
-measure 10 40 true
-measure 10 50 true
-measure 10 80 true
-measure 10 100 true
+measure 20 20
+measure 20 30
+measure 20 40
+measure 20 50
 
-measure 10 20 false
-measure 10 30 false
-measure 10 40 false
-measure 10 50 false
-measure 10 80 false
-measure 10 100 false
-
-measure 20 20 true
-measure 20 30 true
-measure 20 40 true
-measure 20 50 true
-
-measure 20 20 false
-measure 20 30 false
-measure 20 40 false
-measure 20 50 false
+measure 50 5
+measure 50 10
+measure 50 15
