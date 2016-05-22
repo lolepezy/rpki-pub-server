c81bc43ecca8b64bb803e3db571ae2d456424b60
Mikhail Puzanov <mpuzanov@ripe.net>
2016-05-22 23:50:27 +0200

    Add some "toString" for better-looking logging.

--- src/Log.hs
+++ src/Log.hs
@@ -1,21 +1,35 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Log (warn_, error_, info_) where
 
 import           Data.Time.Clock
-import           Data.Time.Format ()
--- import           System.Log.FastLogger
+import           Data.Time.Format      ()
+import           System.Log.FastLogger
 
 -- TODO Implement proper logging using fast-logger
 
-log_ :: Show s => String -> s -> IO ()
+
+-- TODO find a better way to do it
+class ToString a where
+  toString :: a -> String
+
+instance ToString String where toString = id
+instance ToString Char where toString x = [x]
+instance Show a => ToString a where toString = show
+
+
+log_ :: (Show s, ToString s) => String -> s -> IO ()
 log_ level s = do
   t0 <- getCurrentTime
-  print $ show t0 ++ " " ++ level ++ " " ++ show s
+  putStrLn $ show t0 ++ " " ++ level ++ " " ++ toString s
 
-warn_ :: Show s => s -> IO ()
+warn_ :: (Show s, ToString s) => s -> IO ()
 warn_ = log_ "WARN"
 
-info_ :: Show s => s -> IO ()
+info_ :: (Show s, ToString s) => s -> IO ()
 info_ = log_ "INFO"
 
-error_ :: Show s => s -> IO ()
+error_ :: (Show s, ToString s) => s -> IO ()
 error_ = log_ "ERROR"
